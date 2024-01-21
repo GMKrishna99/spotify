8eed8f6fadf23cebf03dc5b80a0da7398ed4b88b
GMKrishna99 <gmohankrishna23@gmail.com>
2024-01-21 11:38:29 +0530

    chore : enables cors & cookieParser , static directory, Ejs

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const cookieParser = require("cookie-parser");
 // initial express app
 
 const express = require("express");
-
 const app = express();
+
+// EJS Settings
+app.set("view engine", "ejs");
+
+// Setting static directory
+app.use(express.static(`${__dirname}/public`));
+
+// Enable cors & cookieParser
+app.use(cors()).use(cookieParser());
 
 // Login Page
 app.get("/login", (req, res) => {
