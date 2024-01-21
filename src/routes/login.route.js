485449c551f34edd476cbb30b672f784691a04d5
GMKrishna99 <gmohankrishna23@gmail.com>
2024-01-21 12:56:22 +0530

    feat : login route

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -2,3 +2,16 @@
  * @license MIT License
  * @copyright gmk 2024
  */
+
+"use strict";
+
+// node modules
+const router = require("express").Router();
+
+// custom modules
+
+const { login } = require("../controllers/login.controller");
+
+router.get("/", login);
+
+module.exports = router;
