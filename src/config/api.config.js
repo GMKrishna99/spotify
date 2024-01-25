8a44e74774ce4a03ce44ba216d3de839943eee52
G_M_Krishna <113287292+GMKrishna99@users.noreply.github.com>
2024-01-25 10:59:04 +0530

    chore : complete configuration file

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -7,3 +7,39 @@ require("dotenv").config();
 
 // the base address of the web api
 const BASE_URL = process.env.BASE_URL;
+
+// The base address of the spotify authentication token
+const TOKEN_BASE_URL = process.env.AUTH_TOKEN;
+
+// spotify client ID
+const CLIENT_ID = process.env.CLIENT_ID;
+
+// spotify client secret
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+// redirect uri for spotify authorization code flow
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+// scope of spotify
+const SCOPE = process.env.SCOPE;
+
+// Authentication state key
+const STATE_KEY = "spotify_auth_state";
+
+// Api requests queries
+const MARKET = "US";
+const LOW_LIMIT = 12;
+const DEFAULT_LIMIT = 28;
+
+module.exports = {
+  BASE_URL,
+  TOKEN_BASE_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI,
+  SCOPE,
+  STATE_KEY,
+  MARKET,
+  LOW_LIMIT,
+  DEFAULT_LIMIT,
+};
